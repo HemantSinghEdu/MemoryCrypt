90494633b57aa2ee55ab9f863babac92a3633c29
HemantSinghEdu <singh.hemant.edu@gmail.com>
2022-01-09 22:28:00 +0530

    Added write operation for MongoDB

--- .NET/Databases/MongoDB/BasicMongoDBApp/Program.cs
+++ .NET/Databases/MongoDB/BasicMongoDBApp/Program.cs
@@ -4,6 +4,7 @@ using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.Conventions;
 using System.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace BasicMongoDBApp
 {
@@ -30,46 +31,86 @@ namespace BasicMongoDBApp
 
     }
 
-
     class Program
     {
         static void Main(string[] args)
         {
-            //MongoDB properties - please provide valid values
-            string username = "user_name";
-            string password = "password";
-            string hostname = "host_name";
-            int portNumber = 12345;
-            string database = "database_name";
-            string replicaSet = "replica_set_name";
-            string collectionName = "collection_name";
-
-            string connectionString = $"mongodb://{username}:{password}@{hostname}:{portNumber}?readPreference=primaryPreferred&replicaSet={replicaSet}&authMechanism=PLAIN&authSource=$external&ssl=true";
-
-            //Ignore the MongoDB fields that are currently not added to our model yet
-            ConventionRegistry.Register(
-                "IgnoreExtraElements",
-                new ConventionPack { new IgnoreExtraElementsConvention(true) },
-                _ => true);
-
-            //Setup the connection
-            IMongoClient mongoClient = new MongoClient(connectionString);
-            IMongoDatabase mongoDatabase = mongoClient.GetDatabase(database);
-            IMongoCollection<Article> mongoCollection = mongoDatabase.GetCollection<Article>(collectionName);
-
-            //fetch the data
-            var records = mongoCollection
-                         .Find(a => a.Author == "John Doe")
-                         .ToEnumerable()
-                         .Where(a => a.Views > 1000);
-
-            //print the records
-            Console.WriteLine("Showing all articles authored by John Doe with more than 1000 views");
-            foreach(var article in records)
+            try
             {
-                Console.WriteLine($"Title: {article.Title} | Content: {article.Content}");
-            }
+                //MongoDB properties - please provide valid values
+                string username = "user_name";  //ignored if connected to localhost
+                string password = "password";   //ignored if connected to localhost
+                string hostname = "localhost";  //server name
+                int portNumber = 27017;
+                string database = "mindcrypt";
+                string replicaSet = "mcrypt_01";
+                string collectionName = "articles";
 
+                string serverConnectionString = $"mongodb://{username}:{password}@{hostname}:{portNumber}?readPreference=primaryPreferred&replicaSet={replicaSet}&authMechanism=PLAIN&authSource=$external&ssl=true";
+                string localConnectionString = $"mongodb://localhost:{portNumber}";
+                string connectionString = hostname == "localhost" ? localConnectionString : serverConnectionString;
+
+                //Ignore the MongoDB fields that are currently not added to our model yet
+                ConventionRegistry.Register(
+                    "IgnoreExtraElements",
+                    new ConventionPack { new IgnoreExtraElementsConvention(true) },
+                    _ => true);
+
+                //Setup the connection
+                IMongoClient mongoClient = new MongoClient(connectionString);
+                IMongoDatabase mongoDatabase = mongoClient.GetDatabase(database);
+                IMongoCollection<Article> mongoCollection = mongoDatabase.GetCollection<Article>(collectionName);
+
+                //write some test data 
+                mongoCollection.InsertMany(testData);
+
+                //fetch the data
+                var records = mongoCollection
+                            .Find(a => a.Author == "John Doe")
+                            .ToEnumerable()
+                            .Where(a => a.Views > 1000);
+
+                //print the records
+                Console.WriteLine("Showing all articles authored by John Doe with more than 1000 views");
+                foreach (var article in records)
+                {
+                    Console.WriteLine($"Title: {article.Title} | Content: {article.Content}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message} | {ex.StackTrace}");
+            }
         }
+
+        private static List<Article> testData = new List<Article>
+        {
+            new Article
+            {
+                Title = "How to Know Who You Really Are and Aren't", 
+                Author = "John Doe", 
+                Content = "What are your most important personal values? Do you actually value what you say you do, or are you lying to yourself? And just who the hell are you anyway?", 
+                Views = 1000,  
+                UpVotes = 200
+            },
+
+            new Article
+            {
+                Title = "Why Facts Don't Change Our Opinions", 
+                Author = "John Doe", 
+                Content = "Why don't facts change our minds? And why would someone continue to believe a false or inaccurate idea anyway? How do such behaviors serve us?", 
+                Views = 1500,  
+                UpVotes = 250
+            },
+
+            new Article
+            {
+                Title = "Physicists discover Higgs boson",
+                Author = "Jane Doe", 
+                Content = "The Higgs boson, discovered at the CERN particle physics laboratory near Geneva, Switzerland, is the particle that gives all other fundamental particles mass",
+                Views = 1000,  
+                UpVotes = 50
+            },
+        };
     }
 }
---
+++ .NET/Databases/MongoDB/Readme.txt
@@ -0,0 +1,3 @@
+For the purpose of running the MongoDB code, you can install MongoDB on your local system or connect to an existing instance.
+If connecting to locally running instance, you can connect to localhost. But ensure that the MongoDB service is up and running.
+You can check the service running status on your task manager, under the services tab.
