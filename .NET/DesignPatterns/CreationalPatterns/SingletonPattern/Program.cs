e7b23b5bcce43d5d8ff1f621afd69de271b04a64
HemantSinghEdu <singh.hemant.edu@gmail.com>
2022-11-24 16:03:45 -0600

    Added Singleton pattern

---
+++ .NET/DesignPatterns/CreationalPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+
+Singleton.Name = "Eager Singleton";
+Console.WriteLine($"Starting Parallel call for : {Singleton.Name}");
+
+Parallel.For(0,10, i =>
+{
+    var singleton = Singleton.Instance;
+    singleton.PrintMessage($"Thread {i}");
+});
+
+SingletonLazy.Name = "Lazy Singleton";
+Console.WriteLine($"Starting Parallel call for : {SingletonLazy.Name}");
+
+Parallel.For(0,10, i =>
+{
+    var singletonLazy = SingletonLazy.Instance;
+    singletonLazy.PrintMessage($"Thread {i}");
+});
+
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
